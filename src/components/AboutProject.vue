702a3865f7af4c78d141c20fc66f22e70fd19557
Christian <chriamue@gmail.com>
2022-03-18 09:48:15 +0100

    :sparkles: added about project component

---
+++ src/components/AboutProject.vue
@@ -0,0 +1,51 @@
+<template>
+  <p id="title">About the project</p>
+  <div id="container">
+    <div id="tile1"></div>
+    <div id="tile2">
+      Imaging &amp; Co. is a library for image processing algorithms. The web
+      page allows to test and use them.
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import HowToTile from "./HowToTile.vue";
+export default {
+  components: {
+    HowToTile,
+  },
+};
+</script>
+<style scoped lang="scss">
+#title {
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 45px;
+  color: #000000;
+}
+#container {
+  display: flex;
+}
+#tile1 {
+  width: 561px;
+  height: 374px;
+
+  border-radius: 17px;
+  background-image: url("~@/assets/about_project_tile1.jpg");
+}
+#tile2 {
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 30px;
+  color: #000000;
+  
+  width: 557px;
+  height: 346px;
+  background: #f3dfe0;
+  background-image: url("~@/assets/about_project_tile2.svg");
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,27 +2,8 @@
   <div>
     <div class="main">
       <h1>Convert images online without upload of your data</h1>
-      <h2 v-if="just">{{ just }}</h2>
-      <div id="process" class="top_main">
-        <ImageUploadPreview
-          :images="resultImages"
-          @changeImage="changeImages($event)"
-        />
-      </div>
-      <div class="bottom_main">
-        <PluginsManager
-          class="v-step-4"
-          ref="pluginsManager"
-          :pluginManager="pluginManager"
-          :defaultConfig="defaultConfig"
-          :defaultPlugins="defaultPlugins"
-          @change="processImages()"
-        />
-      </div>
-      <div id="line">
-        <hr />
-        <div class="plus radius" v-on:click="onMorePlugins()"></div>
-      </div>
+      
+      <AboutProject />
       <HowToConvert />
     </div>
   </div>
@@ -30,122 +11,19 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import * as NProgress from "nprogress";
-import { useToast } from "vue-toastification";
-import PluginsManager from "../components/PluginsManager.vue";
-import ImageUploadPreview from "../components/ImageUploadPreview.vue";
+import AboutProject from "../components/AboutProject.vue";
 import HowToConvert from "../components/HowToConvert.vue";
-import { PluginManager } from "../controllers/plugin_manager";
-import { Image, ImageSource } from "../models/image";
-import { processImages } from "../controllers/processor";
-import { listPlugins } from "../controllers/storage";
-
-export declare interface HomeData {
-  pluginUrl: string;
-  just: string | null;
-  defaultConfig: [];
-  defaultPlugins: string[];
-  sourceImages: Image[];
-  resultImages: ImageSource[];
-  pluginManager: PluginManager;
-}
 
 export default defineComponent({
   name: "Home",
   components: {
-    PluginsManager,
-    ImageUploadPreview,
+    AboutProject,
     HowToConvert,
-  },
-  data(): HomeData {
-    return {
-      pluginUrl: "",
-      just: null,
-      defaultConfig: [],
-      defaultPlugins: [],
-      sourceImages: [],
-      resultImages: [],
-      pluginManager: new PluginManager(""),
-    };
-  },
-  setup: () => {
-    const pluginsManager = ref(PluginsManager);
-    return {
-      pluginsManager,
-    };
-  },
-  created() {
-    if (this.$route.query["config"]) {
-      this.defaultConfig = JSON.parse(
-        // eslint-disable-next-line no-undef
-        Buffer.from(this.$route.query["config"].toString(), "base64").toString(
-          "binary"
-        )
-      );
-      console.log(this.defaultConfig);
-    }
-    if (this.$route.query.plugin) {
-      this.defaultPlugins = [this.$route.query.plugin.toString()];
-    }
-    if (this.$route.query.just) {
-      this.pluginManager.filter = this.$route.query.just.toString();
-      this.just = this.$route.query.just.toString();
-    }
-    this.loadDefaultPluginsMap();
-    this.loadDefaultPluginJson();
-    this.loadAddedPlugins();
-  },
-  methods: {
-    loadPlugin(pluginUrl: string) {
-      console.log("loaded plugin: ", pluginUrl);
-      this.pluginManager.importPlugin(pluginUrl, pluginUrl);
-    },
-
-    loadDefaultPluginsMap() {
-      this.pluginManager.importPluginMap(
-        "https://lenna.app/lenna-plugins/importmap.json"
-      );
-    },
-    loadDefaultPluginJson() {
-      this.pluginManager.importPluginsJson(
-        "https://lenna.app/lenna-plugins/plugins.json"
-      );
-    },
-    loadAddedPlugins() {
-      listPlugins().forEach((plugin) => {
-        this.pluginManager.importPlugin(plugin, plugin);
-      });
-    },
-    onMorePlugins() {
-      window.location.replace("/marketplace");
-    },
-    changeImages(files: any) {
-      this.sourceImages = [files.file];
-      this.processImages();
-    },
-    async processImages() {
-      NProgress.configure({ parent: "#process" });
-      NProgress.start();
-
-      await processImages(
-        this.sourceImages,
-        this.resultImages,
-        this.pluginManager.getPlugins(),
-        {
-          info: (message: string) => {},
-          success: (message: string) => {},
-        },
-        NProgress.set
-      );
-      NProgress.done();
-      NProgress.remove();
-    },
-  },
+  }
 });
 </script>
 <style scoped lang="scss">
 @import "@/styles/_variables.scss";
-@import "//unpkg.com/nprogress@0.2.0/nprogress.css";
 @import url("https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap");
 
 .main {
@@ -175,45 +53,6 @@ export default defineComponent({
 .bottom_main {
   padding-top: 20px;
   padding-bottom: 0;
-}
-#process button {
-  margin-top: 40%;
-  width: 350px;
-  align-self: center;
-  font-size: 20pt;
-  text-transform: uppercase;
-  padding: 12px;
-  border-radius: 30px;
-  border: none;
-  box-shadow: 5px 5px 5px $shadow;
-}
-#process button:hover {
-  transform: scale(1.05);
-}
-#line {
-  margin-top: 50px;
-  position: relative;
-}
-
-.plus {
-  position: absolute;
-  top: -25px;
-  left: calc(50% - 20px);
-
-  display: inline-block;
-  width: 50px;
-  height: 50px;
-
-  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff), blue;
-  background-position: center;
-  background-size: 50% 2px, 2px 50%; /*thickness = 2px, length = 50% (25px)*/
-  background-repeat: no-repeat;
-}
-.radius {
-  border-radius: 50%;
-}
-.plus:hover {
-  transform: scale(1.1);
 }
 
 @media screen and (max-width: 800px) {
