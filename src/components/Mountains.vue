f3a8ac7b657936a872a5920b27a315c9c41d80b8
Christian <chriamue@gmail.com>
2022-03-19 11:31:12 +0100

    :sparkles: added Mountains component

---
+++ src/components/Mountains.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="container">
+    <span id="mountain1" />
+    <div id="container-right">
+      <span id="mountain2" />
+      <span id="mountain3" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+</script>
+<style scoped lang="css">
+#container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#container-right {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+#mountain1 {
+  width: 480px;
+  height: 320px;
+  background: url("../assets/mountain1.jpg");
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 17px;
+}
+
+#mountain2 {
+  margin: 5px;
+  width: 224px;
+  height: 150px;
+  background: url("../assets/mountain2.jpg");
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 14px;
+}
+
+#mountain3 {
+  margin: 5px;
+  width: 224px;
+  height: 150px;
+  background: url("../assets/mountain3.jpg");
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 14px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div>
     <div class="main">
       <h1>Convert images online without upload of your data</h1>
-      
+      <Mountains />
+      <router-link class="process-image-button" to="/batch">Process Image</router-link>
       <AboutProject />
       <HowToConvert />
     </div>
@@ -11,15 +12,17 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import Mountains from "../components/Mountains.vue";
 import AboutProject from "../components/AboutProject.vue";
 import HowToConvert from "../components/HowToConvert.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
+    Mountains,
     AboutProject,
     HowToConvert,
-  }
+  },
 });
 </script>
 <style scoped lang="scss">
@@ -31,6 +34,9 @@ export default defineComponent({
   padding: 150px;
   padding-top: 120px;
   text-align: center;
+  background-image: url("../assets/mainbg.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
 }
 .main h1 {
   text-transform: uppercase;
@@ -53,6 +59,25 @@ export default defineComponent({
 .bottom_main {
   padding-top: 20px;
   padding-bottom: 0;
+}
+
+.process-image-button {
+  display: inline-block;
+  text-decoration: inherit;
+  margin: 40px;
+  width: 248px;
+  padding: 25px;
+
+  background: #d8c6c6;
+  border-radius: 11px;
+
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 30px;
+
+  color: #000000;
 }
 
 @media screen and (max-width: 800px) {
